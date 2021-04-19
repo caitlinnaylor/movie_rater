22a525d67db3c4e8677923b3ddd3eea960c4e394
caitlinnay <60642044+caitlinnay@users.noreply.github.com>
2021-04-19 18:29:46 +1200

    Update Movie Rater.py
    Added fonts and colours

--- Movie Rater.py
+++ Movie Rater.py
@@ -17,29 +17,28 @@ class RatingGUI:
                           Movies("Captain America: The First Avenger", self.default),
                           Movies("Wonder", self.default)]
 
-        self.movie_instruct = Label(parent, text = "Please Rate:", anchor = NW)
+        self.movie_instruct = Label(parent, text = "Please Rate:", anchor = NW, bg = "black", fg = "white", font = ("Sans Serif", 20), pady = 20, padx = 5)
         self.movie_instruct.grid(row = 0, column = 0)
 
-        self.movie_title = Label(parent, text= self.movie_info[0].movie)
-        self.movie_title.grid(row = 0, column = 2)
+        self.movie_title = Label(parent, text= self.movie_info[0].movie, bg = "black", fg = "white", font = ("Sans Serif", 20), anchor = NE, pady = 20, padx = 20)
+        self.movie_title.grid(row = 0, column = 2, columnspan =2)
 
-        self.rating_instruct = Label(parent, text = "Your Rating:", anchor = NW)
+        self.rating_instruct = Label(parent, text = "Your Rating:", anchor = NW, fg = "black", font = ("Sans Serif", 18), pady = 20, bg = "#d4a8ed")
         self.rating_instruct.grid(row = 1, column = 0)
 
         self.var = StringVar()
         self.var.set(0)
         self.num_radiobuttons = 6
-        self.rbs = [Radiobutton(parent, text = self.default, value = self.default, variable = self.var)]
+        self.rbs = [Radiobutton(parent, text = self.default, value = self.default, variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 20, bg = "#d4a8ed")]
 
         for i in range(self.num_radiobuttons):
-            self.rbs.append(Radiobutton(parent, text = f"{i+1}/5", value = f"{i+1}/5", variable = self.var))
+            self.rbs.append(Radiobutton(parent, text = f"{i+1}/5", value = f"{i+1}/5", variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 10, bg = "#d4a8ed"))
             self.rbs[i].grid(row = i+1, column = 2, sticky = NW)
+        self.nextbtn = Button(parent, text = "Next", bg = "#801bb3", fg = "white", font = ("Sans Serif", 14))
+        self.nextbtn.grid(row = 7, column = 3, sticky = NE)
 
-        self.nextbtn = Button(parent, text = "Next")
-        self.nextbtn.grid(row = 7, column = 3)
-
-        self.prevbtn = Button(parent, text = "Previous")
-        self.prevbtn.grid(row = 7, column = 0)
+        self.prevbtn = Button(parent, text = "Previous", bg = "#801bb3", fg = "white", font = ("Sans Serif", 14))
+        self.prevbtn.grid(row = 7, column = 0, sticky = NW)
 
         
               
@@ -47,7 +46,8 @@ class RatingGUI:
 #main
 if __name__=="__main__":
     root= Tk()
-    quiz = RatingGUI(root, Movies, "No Rating")
+    ratings = RatingGUI(root, Movies, "No Rating")
     root.title("Movie Rater")
+    root.configure(bg = "#d4a8ed")
     root.mainloop()
 
