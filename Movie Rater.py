eaf6c40b1594bd5fec5bc5759b840fea405b84c8
caitlinnay <60642044+caitlinnay@users.noreply.github.com>
2021-04-25 22:49:32 +1200

    Update Movie Rater.py
    Search Layout

--- Movie Rater.py
+++ Movie Rater.py
@@ -17,7 +17,7 @@ class RatingGUI:
         self.default = default
         self.i = 0
         self.movie_info = [Movies("The Hobbit", self.default),
-                          Movies("Captain America: The First Avenger", self.default),
+                          Movies("Thor Ragnarok", self.default),
                           Movies("Wonder", self.default),
                            Movies("The Lion King", self.default),
                            Movies("Mary Poppins", self.default),
@@ -30,22 +30,22 @@ class RatingGUI:
                            Movies("The Martian", self.default),
                            Movies("Ant-Man", self.default)]
 
-        self.movie_instruct = Label(f1, text = "Please Rate:", anchor = NW, bg = "black", fg = "white", font = ("Sans Serif", 20), pady = 20, padx = 20)
+        self.movie_instruct = Label(f1, text = "Please Rate:", anchor = NW, bg = "black", fg = "white", font = ("Sans Serif", 20), width = 15, pady = 20, padx = 20)
         self.movie_instruct.grid(row = 0, column = 0)
 
-        self.movie_title = Label(f1, text= self.movie_info[self.i].movie, bg = "black", fg = "white", font = ("Sans Serif", 20), anchor = NE, pady = 20, padx = 70)
+        self.movie_title = Label(f1, text= self.movie_info[self.i].movie, bg = "black", fg = "white", font = ("Sans Serif", 20), anchor = NW, pady = 20, padx = 20, width = 18)
         self.movie_title.grid(row = 0, column = 1, columnspan = 3)
 
-        self.rating_instruct = Label(f1, text = "Your Rating:", anchor = NW, fg = "black", font = ("Sans Serif", 18), pady = 20, bg = "#d4a8ed")
-        self.rating_instruct.grid(row = 1, column = 0)
+        self.rating_instruct = Label(f1, text = "Your Rating:", anchor = NW, fg = "black", font = ("Sans Serif", 18), pady = 20, bg = "#d4a8ed", padx = 20)
+        self.rating_instruct.grid(row = 1, column = 0, sticky = NW)
 
         self.var = StringVar()
         self.var.set(0)
         self.num_radiobuttons = 6
         self.rbs = [Radiobutton(f1, text = self.default, value = self.default, variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 20, bg = "#d4a8ed")]
 
-        self.space_holder_temporary = Label(f1, text = "", padx = 20, bg ="#d4a8ed" )
-        self.space_holder_temporary.grid(row = 1, column = 1)
+        self.space_holder= Label(f1, text = "", padx = 20, bg ="#d4a8ed" )
+        self.space_holder.grid(row = 1, column = 1)
 
         for i in range(self.num_radiobuttons):
             self.rbs.append(Radiobutton(f1, text = f"{i+1}/5", value = f"{i+1}/5", variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 10, bg = "#d4a8ed"))
@@ -59,14 +59,34 @@ class RatingGUI:
         f1.grid(column = 0, row = 0)
 
         f2 = Frame(parent)
-        f2.configure(bg = "#500178")
+        f2.configure(bg = "#500178", pady = 10)
 
-        search_instruct = Label(f2, text = "Search for movies with a rating of:", fg = "white", bg = "#500178", font = ("Sans Serif", 18))
-        search_instruct.grid(row = 8, column = 0, columnspan = 4)
+        self.var_two = StringVar()
+        self.var_two.set(0)
 
+        self.search_instruct = Label(f2, text = "Search for movies with a rating of:", fg = "white", bg = "#500178", font = ("Sans Serif", 18), padx = 54, width = 36)
+        self.search_instruct.grid(row = 0, column = 0, columnspan = 4, sticky  = NW)
 
+        self.search_rbs = [Radiobutton(f2, text = self.default, value = self.default, variable = self.var_two, fg = "white", font = ("Sans Serif", 14), padx = 20,bg = "#500178", pady = 10)]
+
+        self.num_search_radiobuttons = 3 #in in row
+        for i in range(self.num_search_radiobuttons-1):
+            self.search_rbs.append(Radiobutton(f2, text = f"{i+1}/5", value = f"{i+1}/5", variable = self.var_two, fg = "white", pady = 10, padx = 20, font = ("Sans Serif", 14), bg = "#500178"))
+
+        for i in range(self.num_search_radiobuttons):
+            self.search_rbs.append(Radiobutton(f2, text = f"{i+3}/5", value = f"{i+4}/5", variable = self.var_two, fg = "white", pady = 10, padx = 20, font = ("Sans Serif", 14), bg = "#500178"))
+
+        for i in range(self.num_radiobuttons):
+            if i <=2:
+                self.search_rbs[i].grid(row = 1, column = i, sticky = NW)
+            else:
+                self.search_rbs[i].grid(row = 2, column = i-3, sticky = NW)
+
+        self.searchbtn = Button(f2, text = "Go", bg = "white", fg = "black", font = ("Sans Serif", 14))
+        self.searchbtn.grid(row = 11, column = 3, sticky = NW)
 
         f2.grid(column = 0, row = 8)
+        
     def nextmovie(self):
         if self.var.get() != "0":
             self.movie_info[self.i].rating = self.var.get()
