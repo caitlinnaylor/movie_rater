f92177edfc88fa35a25f0a0c27b547f7ccc0b8d3
caitlinnay <60642044+caitlinnay@users.noreply.github.com>
2021-04-25 18:04:52 +1200

    Update Movie Rater.py
    Added frames

--- Movie Rater.py
+++ Movie Rater.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#21/04/21
+#25/04/21
 #Caitlin Naylor
 #Movie Rater
 
@@ -12,6 +12,8 @@ class Movies:
 
 class RatingGUI:
     def __init__(self, parent, Movies, default):
+        f1 = Frame(parent)
+        f1.configure(bg = "#d4a8ed")
         self.default = default
         self.i = 0
         self.movie_info = [Movies("The Hobbit", self.default),
@@ -28,33 +30,43 @@ class RatingGUI:
                            Movies("The Martian", self.default),
                            Movies("Ant-Man", self.default)]
 
-        self.movie_instruct = Label(parent, text = "Please Rate:", anchor = NW, bg = "black", fg = "white", font = ("Sans Serif", 20), pady = 20, padx = 20)
+        self.movie_instruct = Label(f1, text = "Please Rate:", anchor = NW, bg = "black", fg = "white", font = ("Sans Serif", 20), pady = 20, padx = 20)
         self.movie_instruct.grid(row = 0, column = 0)
 
-        self.movie_title = Label(parent, text= self.movie_info[self.i].movie, bg = "black", fg = "white", font = ("Sans Serif", 20), anchor = NE, pady = 20, padx = 70)
+        self.movie_title = Label(f1, text= self.movie_info[self.i].movie, bg = "black", fg = "white", font = ("Sans Serif", 20), anchor = NE, pady = 20, padx = 70)
         self.movie_title.grid(row = 0, column = 1, columnspan = 3)
 
-        self.rating_instruct = Label(parent, text = "Your Rating:", anchor = NW, fg = "black", font = ("Sans Serif", 18), pady = 20, bg = "#d4a8ed")
+        self.rating_instruct = Label(f1, text = "Your Rating:", anchor = NW, fg = "black", font = ("Sans Serif", 18), pady = 20, bg = "#d4a8ed")
         self.rating_instruct.grid(row = 1, column = 0)
 
         self.var = StringVar()
         self.var.set(0)
         self.num_radiobuttons = 6
-        self.rbs = [Radiobutton(parent, text = self.default, value = self.default, variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 20, bg = "#d4a8ed")]
+        self.rbs = [Radiobutton(f1, text = self.default, value = self.default, variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 20, bg = "#d4a8ed")]
 
-        self.space_holder_temporary = Label(parent, text = "", padx = 20, bg ="#d4a8ed" )
+        self.space_holder_temporary = Label(f1, text = "", padx = 20, bg ="#d4a8ed" )
         self.space_holder_temporary.grid(row = 1, column = 1)
 
         for i in range(self.num_radiobuttons):
-            self.rbs.append(Radiobutton(parent, text = f"{i+1}/5", value = f"{i+1}/5", variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 10, bg = "#d4a8ed"))
+            self.rbs.append(Radiobutton(f1, text = f"{i+1}/5", value = f"{i+1}/5", variable = self.var, fg = "black", font = ("Sans Serif", 14), pady = 10, bg = "#d4a8ed"))
             self.rbs[i].grid(row = i+1, column = 2, sticky = NW)
-        self.nextbtn = Button(parent, text = "Next", bg = "#801bb3", fg = "white", font = ("Sans Serif", 14), command = self.nextmovie)
+        self.nextbtn = Button(f1, text = "Next", bg = "#801bb3", fg = "white", font = ("Sans Serif", 14), command = self.nextmovie)
         self.nextbtn.grid(row = 7, column = 3, sticky = NE)
 
-        self.prevbtn = Button(parent, text = "Previous", bg = "#801bb3", fg = "white", font = ("Sans Serif", 14), command = self.prevmovie)
+        self.prevbtn = Button(f1, text = "Previous", bg = "#801bb3", fg = "white", font = ("Sans Serif", 14), command = self.prevmovie)
         self.prevbtn.grid(row = 7, column = 0, sticky = NW)
 
+        f1.grid(column = 0, row = 0)
 
+        f2 = Frame(parent)
+        f2.configure(bg = "#500178")
+
+        search_instruct = Label(f2, text = "Search for movies with a rating of:", fg = "white", bg = "#500178", font = ("Sans Serif", 18))
+        search_instruct.grid(row = 8, column = 0, columnspan = 4)
+
+
+
+        f2.grid(column = 0, row = 8)
     def nextmovie(self):
         if self.var.get() != "0":
             self.movie_info[self.i].rating = self.var.get()
@@ -82,6 +94,5 @@ if __name__=="__main__":
     root= Tk()
     ratings = RatingGUI(root, Movies, "No Rating")
     root.title("Movie Rater")
-    root.configure(bg = "#d4a8ed")
     root.mainloop()
 
